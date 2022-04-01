bd265b07490cc839ba65abf2f5ac4f342950bac0
99009744 <55435447+99009744@users.noreply.github.com>
2022-04-01 10:09:33 +0200

    Created files to add css/js
    Installed node.js
    Installed npm
    Created and linked css file
    Created and linked js file
    Tested js files
    Tested css file

--- jukebox/src/Controller/JukeboxController.php
+++ jukebox/src/Controller/JukeboxController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Song;
 use App\Repository\SongRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -11,28 +12,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class JukeboxController extends AbstractController
 {
-    // private $em;
-    // public function __construct(EntityManagerInterface $em)
-    // {
-    //     $this->em = $em;
-    // }
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
 
     #[Route('/jukebox', name: 'jukebox')]
-    public function index(SongRepository $songRepository): Response
+    public function index(): Response
     {
         // findAll()  - SELECT * from songs.
         // find(value) - SELECT * from songs WHERE id = value.
         // findby([], ['id' => DESC]) - Select * from songs ORDER BY id DESC.
         // findOneBy(['id' => 5,'name' -> 'stay'] ['id' => 'DESC']) 2nd is optional if you want te DESC add it, 1st is optional how many values you want. SELECT * from songs WHERE is = 5 AND name = 'Stay' ORDDER by id DESC
         // count([]) counts the repositorys. - SELECT COUNT() from songs.
-
-        $songs = $songRepository->findAll();
-        dd($songs); 
-
-    //    $repository = $this->em->getRepository(Song::class);
-    //    $songs = $repository->findAll();
+       $repository = $this->em->getRepository(Song::class);
+       $songs = $repository->findAll();
        
-    //     dd($songs);
+        
 
         return $this->render('index.html.twig');
     }
