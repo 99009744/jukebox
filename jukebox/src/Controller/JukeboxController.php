682cec84e25a794f5c8cdc5622f3fef66593380d
99009744 <55435447+99009744@users.noreply.github.com>
2022-04-04 13:49:04 +0200

    Started CRUD
    Created the read.

--- jukebox/src/Controller/JukeboxController.php
+++ jukebox/src/Controller/JukeboxController.php
@@ -12,13 +12,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class JukeboxController extends AbstractController
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em)
+    // private $em;
+    // public function __construct(EntityManagerInterface $em)
+    // {
+    //     $this->em = $em;
+    // }
+
+    private $songRepository;
+    public function __construct(SongRepository $songRepository)
     {
-        $this->em = $em;
+        $this->songRepository = $songRepository;
     }
 
-    #[Route('/jukebox', name: 'jukebox')]
+    #[Route('/jukebox', methods: ['GET'], name: 'jukebox')]
     public function index(): Response
     {
         // findAll()  - SELECT * from songs.
@@ -26,11 +32,21 @@ class JukeboxController extends AbstractController
         // findby([], ['id' => DESC]) - Select * from songs ORDER BY id DESC.
         // findOneBy(['id' => 5,'name' -> 'stay'] ['id' => 'DESC']) 2nd is optional if you want te DESC add it, 1st is optional how many values you want. SELECT * from songs WHERE is = 5 AND name = 'Stay' ORDDER by id DESC
         // count([]) counts the repositorys. - SELECT COUNT() from songs.
-       $repository = $this->em->getRepository(Song::class);
-       $songs = $repository->findAll();
-       
+    //    $repository = $this->em->getRepository(Song::class);
+    //    $songs = $repository->findAll();
         
+        return $this->render('jukebox/index.html.twig', [
+            'songs' => $this->songRepository->findAll()
+        ]);
+    }
 
-        return $this->render('index.html.twig');
+    #[Route('/jukebox/{id}', methods: ['GET'], name: 'song')]
+    public function show($id): Response
+    {
+        $song = $this->songRepository->find($id);
+
+        return $this->render('jukebox/show.html.twig', [
+            'song' => $song
+        ]);
     }
 }
